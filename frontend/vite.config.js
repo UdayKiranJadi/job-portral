572d912562a80f0182e4c0c29d422e5ff9dd9e1e
Uday Kiran Jadi <udayjadi4@gmail.com>
2026-02-21 15:16:55 -0800

    NavBar

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,12 @@
-import path from "path"
-import { fileURLToPath } from "url"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
-import tailwindcss from "@tailwindcss/vite"
+import path from "path";
+import { fileURLToPath } from "url";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -15,4 +14,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
