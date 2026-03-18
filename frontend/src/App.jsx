b9aeee5008765257bda82bf1783cfc2bc36b335f
Uday Kiran Jadi <udayjadi4@gmail.com>
2026-03-17 21:27:49 -0700

    chages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 
 import { createBrowserRouter } from 'react-router-dom';
 import Navbar from './components/ui/shared/Navbar';
-import Login from './components/ui/auth/login';
-import Signup from './components/ui/auth/signup';
+import Login from './components/ui/auth/Login';
+import Signup from './components/ui/auth/Signup';
 import Home from './components/ui/Home';
 import { RouterProvider } from "react-router-dom";
 import Jobs from './components/ui/Jobs';
