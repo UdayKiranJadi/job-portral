0694b66d226ec55f5949385ad836356aaa85af27
Uday Kiran Jadi <udayjadi4@gmail.com>
2026-03-22 19:04:07 -0700

    user

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -86,7 +86,12 @@ export const login = async (req, res) => {
             profile: user.profile
         }
 
-        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpsOnly: true, sameSite: 'strict' }).json({
+        return res.status(200).cookie("token", token, {
+  maxAge: 1 * 24 * 60 * 60 * 1000,
+  httpOnly: true,
+  sameSite: "none",
+  secure: true,
+}).json({
             message: `welcome back ${user.fullName}`,
             user,
             success: true
